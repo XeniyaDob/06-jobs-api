a5a616e02061d6e0b1fa050a4bb0a078b9f5c900
XeniyaDob <xeniya.dobrogorskaya@gmail.com>
2024-04-08 15:49:35 -0400

    Connect to the DB

--- app.js
+++ app.js
@@ -4,13 +4,14 @@ const express = require('express');
 const app = express();
 
 //connect DB
+const connectDB = require("./db/connect");
 
 //routers
-const authRouter = require("./routes/auth")
-const itemsRouter = require("./routes/items")
+const authRouter = require("./routes/auth");
+const itemsRouter = require("./routes/items");
 // error handler
-const notFoundMiddleware = require('./middleware/not-found');
-const errorHandlerMiddleware = require('./middleware/error-handler');
+const notFoundMiddleware = require("./middleware/not-found");
+const errorHandlerMiddleware = require("./middleware/error-handler");
 
 app.use(express.json());
 // extra packages
@@ -26,6 +27,7 @@ const port = process.env.PORT || 3000;
 
 const start = async () => {
   try {
+    await connectDB(process.env.MONGO_URI);
     app.listen(port, () =>
       console.log(`Server is listening on port ${port}...`)
     );
