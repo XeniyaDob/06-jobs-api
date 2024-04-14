f2cc4e626e3687d57b994b68a6f970cbbded7988
XeniyaDob <xeniya.dobrogorskaya@gmail.com>
2024-04-14 11:21:30 -0400

    Add a simple check if app is life and create a .node-version file

--- app.js
+++ app.js
@@ -36,6 +36,10 @@ app.use(helmet());
 app.use(cors());
 app.use(xss());
 
+app.get("/", (req, res) => {
+  res.send("Records API");
+});
+
 // routes
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/items", authenticateUser, itemsRouter);
