69fe5cdc465d7676b8c4a00d8102eae25ece323e
XeniyaDob <xeniya.dobrogorskaya@gmail.com>
2024-04-18 16:58:29 -0400

    GET and display plants from DB

--- public/jobs.js
+++ public/jobs.js
@@ -10,15 +10,15 @@ import { showLoginRegister } from "./loginRegister.js";
 import { showAddEdit } from "./addEdit.js";
 
 let jobsDiv = null;
-let jobsTable = null;
-let jobsTableHeader = null;
+let itemsTable = null;
+let itemsTableHeader = null;
 
 export const handleJobs = () => {
   jobsDiv = document.getElementById("jobs");
   const logoff = document.getElementById("logoff");
   const addItem = document.getElementById("add-item");
-  jobsTable = document.getElementById("jobs-table");
-  jobsTableHeader = document.getElementById("jobs-table-header");
+  itemsTable = document.getElementById("items-table");
+  itemsTableHeader = document.getElementById("items-table-header");
 
   jobsDiv.addEventListener("click", (e) => {
     if (inputEnabled && e.target.nodeName === "BUTTON") {
@@ -29,7 +29,7 @@ export const handleJobs = () => {
 
         message.textContent = "You have been logged off.";
 
-        jobsTable.replaceChildren([jobsTableHeader]);
+        itemsTable.replaceChildren([itemsTableHeader]);
 
         showLoginRegister();
       }
@@ -38,5 +38,48 @@ export const handleJobs = () => {
 };
 
 export const showItems = async () => {
+  try {
+    enableInput(false);
+
+    const response = await fetch("/api/v1/items", {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+    });
+
+    const data = await response.json();
+    let children = [itemsTableHeader];
+
+    if (response.status === 200) {
+      if (data.count === 0) {
+        itemsTable.replaceChildren(...children); // clear this for safety
+      } else {
+        for (let i = 0; i < data.items.length; i++) {
+          let rowEntry = document.createElement("tr");
+
+          let editButton = `<td><button type="button" class="editButton" data-id=${data.items[i]._id}>edit</button></td>`;
+          let deleteButton = `<td><button type="button" class="deleteButton" data-id=${data.items[i]._id}>delete</button></td>`;
+          let rowHTML = `
+            <td>${data.items[i].name}</td>
+            <td>${data.items[i].price}</td>
+            <td>${data.items[i].description}</td>
+            <td>${data.items[i].type}</td>
+            <div>${editButton}${deleteButton}</div>`;
+
+          rowEntry.innerHTML = rowHTML;
+          children.push(rowEntry);
+        }
+        itemsTable.replaceChildren(...children);
+      }
+    } else {
+      message.textContent = data.msg;
+    }
+  } catch (err) {
+    console.log(err);
+    message.textContent = "A communication error occurred.";
+  }
+  enableInput(true);
   setDiv(jobsDiv);
 };
